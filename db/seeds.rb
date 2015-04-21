d4a2375d6e1ab12b56318bbcdacf6dd5a534f6b2
Luke Cassar <lukecassar7@gmail.com>
2015-04-21 22:27:46 +0000

    had to chnage seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 sentences = Sentence.create(
-  EnglishSentence: "The rats go through the house.", 
-  EnglishPhonetic: "ðiː ɹæts ɡəʊ θɹuː ðiː haʊs",
-  GermanSentence: "Die Ratten gehen durch das Haus.", 
-  GermanPhonetic: "diː ˈʀatənˈɡeːən dʊʁç das haʊ̯s." , 
-  JapaneseSentence: "ラットは、家の中を行く。",
-  JapaneseRomaji:"Ratto wa, uchinonaka o iku.",
+  english_sentence: "The rats go through the house.", 
+  english_phonetic: "ðiː ɹæts ɡəʊ θɹuː ðiː haʊs",
+  german_sentence: "Die Ratten gehen durch das Haus.", 
+  german_phonetic: "diː ˈʀatənˈɡeːən dʊʁç das haʊ̯s." , 
+  japanese_sentence: "ラットは、家の中を行く。",
+  japanese_romaji:"Ratto wa, uchinonaka o iku.",
   image: "Rats in House.jpg")
 
 sentences = Sentence.create(
-  EnglishSentence: "Hello, my name is Luke.", 
-  EnglishPhonetic: "hɛˈləʊ̯, maɪ neɪm ɪz luk",
-  GermanSentence: "Hallo, mein Name ist Luke.", 
-  GermanPhonetic: "haˈloː, maɪ̯n ˈnaːmə ɪst ˈluːkə." , 
-  JapaneseSentence: "こんにちは、私の名前はルカです。",
-  JapaneseRomaji: "Kon'nichiwa, watashinonamaeha Rukadesu.",
+  english_sentence: "Hello, my name is Luke.", 
+  english_phonetic: "hɛˈləʊ̯, maɪ neɪm ɪz luk",
+  german_sentence: "Hallo, mein Name ist Luke.", 
+  german_phonetic: "haˈloː, maɪ̯n ˈnaːmə ɪst ˈluːkə." , 
+  japanese_sentence: "こんにちは、私の名前はルカです。",
+  japanese_romaji: "Kon'nichiwa, watashinonamaeha Rukadesu.",
   image: "Luke.jpg")
 
 sentences = Sentence.create(
-  EnglishSentence: "I live in Edinburgh.", 
-  EnglishPhonetic: "aɪ lɪv ɪn ˈɛdɪnbərə",
-  GermanSentence: "Ich lebe in Edinburgh.", 
-  GermanPhonetic: "ɪç ˈleːbə ɪn ˈɛdɪnbərə." , 
-  JapaneseSentence: "私は、エジンバラに住んでいます。",
-  JapaneseRomaji: "Watashi wa, ejinbara ni sunde imasu.",
+  english_sentence: "I live in Edinburgh.", 
+  english_phonetic: "aɪ lɪv ɪn ˈɛdɪnbərə",
+  german_sentence: "Ich lebe in Edinburgh.", 
+  german_phonetic: "ɪç ˈleːbə ɪn ˈɛdɪnbərə." , 
+  japanese_sentence: "私は、エジンバラに住んでいます。",
+  japanese_romaji: "Watashi wa, ejinbara ni sunde imasu.",
   image: "Edinburgh.jpg")
