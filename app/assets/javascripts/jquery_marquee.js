387c025aeffe5771852035880e4db9cd34761312
Luke Cassar <lukecassar7@gmail.com>
2015-04-24 09:41:51 +0000

    changed UI, headings, and buttons

--- app/assets/javascripts/jquery_marquee.js
+++ app/assets/javascripts/jquery_marquee.js
@@ -1,33 +1,55 @@
 var ready;
 ready = function() {
 
-var $mq = $('.marquee').marquee();
+var $mq = $('.marquee').marquee({
+   //speed in milliseconds of the marquee
+    duration: 7500,
+});
 
-//Pause
-$('.marquee').hover(function(){
+
+//Stop Start
+$('#stop-start').click(function(){
   $mq.marquee('toggle');
 });
 
 //HideShow Languages
 
-$('#german').click(function(){
-   $('.german').toggle();
-});
-
-$('#japanese').click(function(){
-   $('.japanese').toggle();
-});
-
-$('#portuguese').click(function(){
-   $('.portuguese').toggle();
+$('#target-language').click(function(){
+   $('.target-language').toggle();
 });
 
 $('#english').click(function(){
    $('.english').toggle();
 });
 
+//Toggle button text
+
+$(function(){
+   $("#stop-start").click(function () {
+      $(this).text(function(i, text){
+          return text === "Stop" ? "Start" : "Stop";
+      });
+   });
+});
+
+$(function(){
+   $("#target-language").click(function () {
+      $("#target-language-toggle").text(function(i, text){
+          return text === "On" ? "Off" : "On";
+      });
+   });
+});
+
+$(function(){
+   $("#english").click(function () {
+      $("#english-toggle").text(function(i, text){
+          return text === "On" ? "Off" : "On";
+      });
+   });
+});
+
 
 };
-
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get    'german'   => 'static_pages#german'
   get    'portuguese'   => 'static_pages#portuguese'
   root 'static_pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
